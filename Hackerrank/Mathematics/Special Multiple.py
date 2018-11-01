2dc695254e6cebfbb20a2699bcfe71bce2f48a94
aswanthkoleri <aswanth9495@gmail.com>
2018-11-01 11:18:46 +0530

    Add special multiple.py

--- Hackerrank/Mathematics/Special Multiple.py
+++ Hackerrank/Mathematics/Special Multiple.py
@@ -1,20 +1,37 @@
+def solve(n,d):
+    s=[]
+    s.append(d)
+    while s:
+        new=s
+        s=[]
+        flag=0
+        for i in new:
+            if i%n==0:
+                flag=1
+                answer=i
+                break
+        
+        if flag==1:
+            break
+        else:
+            # Push the new elements to the array
+            for i in new:
+                s.append(int(str(i)+'0'))
+                s.append(int(str(i)+'9'))
+
+    return answer
+        
+
+
+
 def main():
     t=int(input())
     while t:
         n=int(input())
-        num=9
-        if num%n==0:
-            print(num)
-        else:
-            num*=10
-            while 1:
-                print(num)
-                if num%n==0:
-                    print(num)
-                    break
-                elif (num+9)%n==0:
-                    print(num+9)
-                    break
-                num*=10
+        d=9
+        res=solve(n,d)
+        print(res)
         t-=1
+
 main()
+
