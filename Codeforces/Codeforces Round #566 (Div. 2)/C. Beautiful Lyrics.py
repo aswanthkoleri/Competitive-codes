18555039300c24d816627404c086b085e8062575
aswanthkoleri <aswanth9495@gmail.com>
2019-06-12 19:33:33 +0530

    Add codes from a new contest

--- Codeforces/Codeforces Round #566 (Div. 2)/C. Beautiful Lyrics.py
+++ Codeforces/Codeforces Round #566 (Div. 2)/C. Beautiful Lyrics.py
@@ -1,7 +1,7 @@
 if __name__=="__main__":
-	n=int(input())
-	tuples=[]
-	used={}
+	n=int(input())	
+	hashMap={}
+	# Construction of hashMap
 	for _ in range(n):
 		word=input()
 		noOfVowels=0
@@ -10,38 +10,39 @@ if __name__=="__main__":
 			if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
 				last=i
 				noOfVowels+=1
-		tuples.append((word,noOfVowels,last))
-	# print(tuples)
-	noOfLyrics=0
-	answerTuples=[]
-	ans=tuples
-	# ans=sorted(tuples,key=lambda x: x[1],reverse=True)
-	# Split to Arrays containing words with same no of vowels and words containing both same vowel and same last vowel
-	sameVowel=[]
-	sameVowelAndLast=[]
-	used={}
-	for i in range(len(ans)):
-		used[i]=False
-	print(ans)
-	for i in range(len(ans)-1):
-		if not used[i] and not used[i+1] and ans[i][1]==ans[i+1][1] and ans[i][2]==ans[i+1][2]:
-			sameVowelAndLast.append((ans[i],ans[i+1]))
-			used[i]=True
-			used[i+1]=True
-	for i in range(len(ans)):
-		used[i]=False
-	for i in range(len(ans)-1):
-		print(ans[i][0],ans[i+1][0],ans[i][1],ans[i+1][1],used[i],used[i+1])
-		if not used[i] and not used[i+1] and ans[i][1]==ans[i+1][1] and ans[i][2]!= ans[i+1][2]:
-			print(ans[i][0],ans[i+1][0])
-			sameVowel.append((ans[i],ans[i+1]))
-			used[i]=True
-			used[i+1]=True
-
-	# Now to find the no of lyrics
-	print(sameVowelAndLast)
-	print(sameVowel)
-	noOfLyrics=min(len(sameVowel),len(sameVowelAndLast))
-	extra=len(sameVowelAndLast)-noOfLyrics
-	noOfLyrics+=(extra//4)
-	print(noOfLyrics)
+		if noOfVowels not in hashMap:
+			hashMap[noOfVowels]={}
+			hashMap[noOfVowels][last]=[]
+		elif last not in hashMap[noOfVowels]:
+			hashMap[noOfVowels][last]=[]
+		hashMap[noOfVowels][last].append(word)
+	# Constuct complete duos : same NoOfVowels + same LastVowel 
+	CompleteDuos=[]
+	# Constuct Semi-Complete duos : same No Of Vowels
+	SemiCompleteDuos=[]
+	for SameNoOfVowels in hashMap:
+		# Complete Duos
+		remaining=[] # contains the remaining words having the same No of vowels
+		for sameLastVowel in hashMap[SameNoOfVowels]:
+			while len(hashMap[SameNoOfVowels][sameLastVowel])>=2:
+				CompleteDuos.append((hashMap[SameNoOfVowels][sameLastVowel].pop(),hashMap[SameNoOfVowels][sameLastVowel].pop()))
+			while len(hashMap[SameNoOfVowels][sameLastVowel])>0:
+				remaining.append(hashMap[SameNoOfVowels][sameLastVowel].pop())
+		# Semi Complete Duos
+		while len(remaining)>=2:
+			SemiCompleteDuos.append((remaining.pop(),remaining.pop()))
+	# print(CompleteDuos)
+	# print(SemiCompleteDuos)
+	NoOfLyrics=0
+	minValue=min(len(CompleteDuos),len(SemiCompleteDuos))
+	NoOfLyrics=minValue
+	extra=((len(CompleteDuos)-minValue))
+	NoOfLyrics+=extra//2
+	print(NoOfLyrics)
+	# Now printing the output
+	for i in range(minValue):
+		print(SemiCompleteDuos[i][0],CompleteDuos[i][0])
+		print(SemiCompleteDuos[i][1],CompleteDuos[i][1])
+	for j in range(minValue,minValue+extra-1,2):
+		print(CompleteDuos[j][0],CompleteDuos[j+1][0])
+		print(CompleteDuos[j][1],CompleteDuos[j+1][1])
